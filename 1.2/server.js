9cd5f0689011a293080dcfc15b692c228dec7feb
Mirka Väänänen <mirka.vaananen@gmail.com>
2026-09-23 15:15:32 +0300

    add exercise 1.2

---
+++ 1.2/server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
